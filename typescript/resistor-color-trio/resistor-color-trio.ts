86f2ed798b0d9881067e3cbeb0935cb736480386
Felipe <fatamanczuk@degdigital.com>
2022-04-25 08:21:56 -0300

    finished

--- typescript/resistor-color-trio/resistor-color-trio.ts
+++ typescript/resistor-color-trio/resistor-color-trio.ts
@@ -1,3 +1,26 @@
-export function decodedResistorValue() {
-  throw new Error('Remove this statement and implement this function')
+export function decodedResistorValue(resistorColors: string[]):string {
+  interface ResistorColors{
+    [key: string]:string
+  }
+
+  const ResistorColorsMap: ResistorColors = {
+    black: '0',
+    brown: '1',
+    red: '2',
+    orange: '3',
+    yellow: '4',
+    green: '5',
+    blue: '6',
+    violet: '7',
+    grey: '8',
+    white: '9'
+  }
+
+  let ohm = ' kiloohms';
+  const totalValue = parseInt(`${ResistorColorsMap[resistorColors[0]]}${ResistorColorsMap[resistorColors[1]]}`) * 10 ** parseInt(`${ResistorColorsMap[resistorColors[2]]}`);
+  if (totalValue <= 1000) {
+    return totalValue + ' ohms';    
+  }else{
+    return (totalValue/1000)+ ohm;
+  } 
 }
